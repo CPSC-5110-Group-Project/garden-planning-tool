7c54f89c4326556987c7c6fa9a358fdedc5daba4
ryanpage8 <rspage7@gmail.com>
2026-04-20 15:26:02 -0700

    update to pydantic v2 syntax

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     DATABASE_URL: str
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     PERENUAL_API_KEY: str
     WEATHER_API_KEY: str
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
