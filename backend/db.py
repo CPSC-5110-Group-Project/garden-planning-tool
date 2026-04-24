2e6bf65c02bb9ac3447048d2492b2f5cfc3034f5
A.J. Psyk <ajpsyk@gmail.com>
2026-04-23 20:33:44 -0700

    intialize database with pgvector;  ensure extension with health check

--- backend/db.py
+++ backend/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from core.config import settings
@@ -7,6 +7,31 @@ engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+def init_db():
+    with engine.connect() as conn:
+        conn.execute(text("CREATE EXTENSION IF NOT EXISTS vector;"))
+        conn.commit()
+    Base.metadata.create_all(bind=engine)
+
+def get_db_status():
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+            
+            res = conn.execute(text("SELECT extname FROM pg_extension;"))
+            extensions = [row[0] for row in res.fetchall()]
+            
+            return {
+                "status": "connected",
+                "extensions": extensions
+            }
+    except Exception as e:
+        return {
+            "status": "not connected",
+            "message": f"error: {str(e)}",
+            "extensions": []
+        }
+    
 def get_db():
     db = SessionLocal()
     try:
